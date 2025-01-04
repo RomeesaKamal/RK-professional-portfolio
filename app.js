ad152d883eb6db91443f719739767aa5f64c1847
RomeesaKamal <kamalromeesa@gmail.com>
2025-01-04 18:56:33 +0500

    improvment of styles

--- app.js
+++ app.js
@@ -1,37 +1,10 @@
 const closeIcon = document.querySelector("#close");
 const showPopup = document.querySelector("#popup");
 const aboutButton = document.querySelector("#about");
-const phone = document.querySelector("#phone");
-const whatsapp = document.querySelector("#whatsapp");
-const phoneSvg = document.querySelector("#phone-svg");
-const whatsappSvg = document.querySelector("#whatsapp-svg");
+
 
 aboutButton.addEventListener("click", () => {
   showPopup.classList.remove("hidden");
-});
-
-phoneSvg.addEventListener("mouseover", () => {
-  if (window.innerWidth >= 1550) {
-    phone.classList.remove("hidden");
-  }
-});
-
-phoneSvg.addEventListener("mouseout", () => {
-  if (window.innerWidth >= 1550) {
-    phone.classList.add("hidden");
-  }
-});
-
-whatsappSvg.addEventListener("mouseover", () => {
-  if (window.innerWidth >= 1600) {
-    whatsapp.classList.remove("hidden");
-  }
-});
-
-whatsappSvg.addEventListener("mouseout", () => {
-  if (window.innerWidth >= 1600) {
-    whatsapp.classList.add("hidden");
-  }
 });
 
 closeIcon.addEventListener("click", () => {
@@ -62,3 +35,4 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
